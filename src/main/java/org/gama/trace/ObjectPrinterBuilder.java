5dcafdac234a7291d0251b61595c6e6d28726978
Guillaume Mary <guiom.mary@gmail.com>
2020-01-30 23:44:26 +0100

    Adding an experimental object printer to help trace, test, etc

---
+++ src/main/java/org/gama/trace/ObjectPrinterBuilder.java
@@ -0,0 +1,164 @@
+package org.gama.trace;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+
+import org.danekja.java.util.function.serializable.SerializableFunction;
+import org.gama.lang.Experimental;
+import org.gama.lang.Reflections;
+import org.gama.lang.StringAppender;
+import org.gama.lang.bean.InstanceMethodIterator;
+import org.gama.lang.collection.Iterables;
+import org.gama.lang.collection.KeepOrderSet;
+import org.gama.reflection.AccessorByMethod;
+import org.gama.reflection.AccessorByMethodReference;
+import org.gama.reflection.IAccessor;
+import org.gama.reflection.MemberDefinition;
+import org.gama.reflection.ValueAccessPointByMethodReference;
+import org.gama.reflection.ValueAccessPointSet;
+
+/**
+ * Builder for {@link ObjectPrinter}. {@link ObjectPrinter} may be used to give a trace of some instances, to be logged or debug.
+ * Kind of Apache Commons ToStringBuilder with method references.
+ * 
+ * @author Guillaume Mary
+ */
+@Experimental(todo = { "implement recursivity and overall prevent from stackoverflow", "test !" })
+public class ObjectPrinterBuilder<C> {
+	
+	/**
+	 * Starts a printer configurer that will print all (public) methods of given class (including inherited ones).
+	 * Non wished properties may be removed by using {@link #except(SerializableFunction)} on result.
+	 *
+	 * @param type the class which methods must be printed
+	 * @return a {@link ObjectPrinterBuilder} that will print all methods of given class, and may be further configured
+	 */
+	@Experimental(todo = { "remove addProperty from result" })
+	public static <T> ObjectPrinterBuilder<T> printerFor(Class<T> type) {
+		ObjectPrinterBuilder<T> result = new ObjectPrinterBuilder<>();
+		Iterable<Method> methodIterable = () -> new InstanceMethodIterator(type);
+		// we add class getters 
+		for (Method method : methodIterable) {
+			AccessorByMethod<T, Object> accessorByMethod = Reflections.onJavaBeanPropertyWrapperNameGeneric(method.getName(), method,
+					m -> new AccessorByMethod<>(method),
+					m -> null,
+					m -> new AccessorByMethod<>(method),
+					m -> null /* method is not a getter, we exclude it by returning null (filtered below) */);
+			if (accessorByMethod != null) {
+				result.addProperty(accessorByMethod);
+			}
+		}
+		return result;
+	}
+	
+	/** @apiNote  */
+	private final KeepOrderSet<IAccessor<C, Object>> printableProperties = new KeepOrderSet<>();
+	/** @apiNote we use a {@link ValueAccessPointSet} because its supports well contains() method with {@link IAccessor} as argument */
+	private final ValueAccessPointSet excludedProperties = new ValueAccessPointSet();
+	
+	private final Map<Class, Function<Object, String>> overridenPrinters = new HashMap<>();
+	
+	/**
+	 * Adds a property to be printed throught its getter
+	 * 
+	 * @param getter the method reference that gives access to the property, can be one the parameterized class or one of its subtype
+	 * @return this
+	 */
+	public <D extends C> ObjectPrinterBuilder<C> addProperty(SerializableFunction<D, Object> getter) {
+		return this.addProperty(new AccessorByMethodReference(getter));
+	}
+	
+	private ObjectPrinterBuilder<C> addProperty(IAccessor<C, Object> getter) {
+		this.printableProperties.add(getter);
+		return this;
+	}
+	
+	/**
+	 * Excludes a property from being printed, usefull in combination of {@link #printerFor(Class)}
+	 *
+	 * @param getter the method reference that gives access to the property
+	 * @return this
+	 */
+	public ObjectPrinterBuilder<C> except(SerializableFunction<C, Object> getter) {
+		this.excludedProperties.add(new AccessorByMethodReference<>(getter));
+		return this;
+	}
+	
+	/**
+	 * Specifies a printer for a particular type
+	 * 
+	 * @param overridenType the type which printing must be changed
+	 * @param printer the function to use for printing
+	 * @param <E> customized type
+	 * @return this
+	 */
+	public <E> ObjectPrinterBuilder<C> withPrinter(Class<E> overridenType, Function<E, String> printer) {
+		this.overridenPrinters.put(overridenType, (Function<Object, String>) printer);
+		return this;
+	}
+	
+	/**
+	 * Builds final printer
+	 * 
+	 * @return a configured printer for current type
+	 */
+	public ObjectPrinter<C> build() {
+		LinkedHashMap<String, IAccessor<C, Object>> printingFunctionByPropertyName = new LinkedHashMap<>();
+		for (IAccessor<C, Object> printableProperty : printableProperties) {
+			String methodName = MemberDefinition.giveMemberDefinition(printableProperty).getName();
+			if (!excludedProperties.contains(printableProperty)) {
+				printingFunctionByPropertyName.put(methodName, printableProperty);
+			}
+		}
+		return new ObjectPrinter<>(printingFunctionByPropertyName, overridenPrinters);
+	}
+	
+	/**
+	 * Printer for parameterized type
+	 * 
+	 * @param <C> target type to print
+	 */
+	public static class ObjectPrinter<C> {
+		
+		private final Map<String, IAccessor<C, Object>> printableProperties;
+		private final Map<Class, Function<Object, String>> overridenPrinters;
+		
+		/**
+		 * @apiNote private because {@link ObjectPrinterBuilder} is expected to be used for configuration 
+		 */
+		private ObjectPrinter(LinkedHashMap<String, IAccessor<C, Object>> printableProperties, Map<Class, Function<Object, String>> overridenPrinters) {
+			this.printableProperties = printableProperties;
+			this.overridenPrinters = overridenPrinters;
+		}
+		
+		/**
+		 * @param object an instance to be printed
+		 * @return a {@link String} representing given instance according to configured properties to print
+		 */
+		public String toString(C object) {
+			StringAppender result = new StringAppender();
+			String separator = ",";
+			printableProperties.forEach((propName, getter) -> {
+				// we prevent subclass property accessor of being invoked on parent class
+				boolean getterCompliesWithInstance = ((ValueAccessPointByMethodReference) getter).getDeclaringClass().isInstance(object);
+				if (getterCompliesWithInstance) {
+					final Object value = getter.get(object);
+					Entry<Class, Function<Object, String>> foundOverringPrinter = Iterables.find(overridenPrinters.entrySet(),
+							e -> e.getKey().isInstance(value));
+					Object valueToPrint;
+					if (foundOverringPrinter != null) {
+						valueToPrint = foundOverringPrinter.getValue().apply(value);
+					} else {
+						valueToPrint = value;
+					}
+					result.cat(propName, "=", valueToPrint, separator);
+				}
+			});
+			return result.cutTail(separator.length()).toString();
+		}
+	}
+}
---
+++ src/test/java/org/gama/trace/ObjectPrinterBuilderTest.java
@@ -0,0 +1,69 @@
+package org.gama.trace;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Guillaume Mary
+ */
+class ObjectPrinterBuilderTest {
+	
+	@Test
+	void testToString_basic() {
+		ObjectPrinterBuilder<Toto> testInstance = new ObjectPrinterBuilder<Toto>()
+				.addProperty(Toto::getProp1)
+				.addProperty(Toto::getProp2);
+		assertEquals("prop1=Hello,prop2=World", testInstance.build().toString(new Toto().setProp1("Hello").setProp2("World")));
+	}
+	
+	@Test
+	void testToString_excludingProperties() {
+		ObjectPrinterBuilder<Toto> testInstance = new ObjectPrinterBuilder<Toto>()
+				.addProperty(Toto::getProp1)
+				.addProperty(Toto::getProp2)
+				.except(Toto::getProp1);
+		assertEquals("prop2=World", testInstance.build().toString(new Toto().setProp1("Hello").setProp2("World")));
+	}
+	
+	@Test
+	void testToString_overringPrinter() {
+		ObjectPrinterBuilder<Toto> testInstance = new ObjectPrinterBuilder<Toto>()
+				.addProperty(Toto::getProp1)
+				.addProperty(Toto::getProp2)
+				.withPrinter(String.class, s -> "<" + s + ">");
+		assertEquals("prop1=<Hello>,prop2=<World>", testInstance.build().toString(new Toto().setProp1("Hello").setProp2("World")));
+	}
+	
+	@Test
+	void testFrom_excludingProperties() {
+		ObjectPrinterBuilder<Toto> testInstance = ObjectPrinterBuilder.printerFor(Toto.class)
+				.except(Toto::getProp1);
+		assertEquals("prop2=World", testInstance.build().toString(new Toto().setProp1("Hello").setProp2("World")));
+	}
+	
+	private static class Toto {
+		
+		private String prop1;
+		private String prop2;
+		
+		public String getProp1() {
+			return prop1;
+		}
+		
+		public Toto setProp1(String prop1) {
+			this.prop1 = prop1;
+			return this;
+		}
+		
+		public String getProp2() {
+			return prop2;
+		}
+		
+		public Toto setProp2(String prop2) {
+			this.prop2 = prop2;
+			return this;
+		}
+	}
+	
+}
