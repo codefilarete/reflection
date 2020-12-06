c39bdab0b52c19a0df9dc079acbd7eb9f86097ea
Guillaume Mary <guiom.mary@gmail.com>
2020-12-06 10:15:55 +0100

    Javadoc rephrased

--- src/main/java/org/gama/reflection/ValueAccessPoint.java
+++ src/main/java/org/gama/reflection/ValueAccessPoint.java
@@ -1,11 +1,14 @@
 package org.gama.reflection;
 
 /**
- * A marker interface for {@link IMutator} and {@link IAccessor} for use case whenever they must be mixted all together in a generic type such as
- * a {@link java.util.Set} or {@link java.util.Map}.
+ * A marker interface for {@link IMutator} and {@link IAccessor} hence acting as a generic representation of a property accessor.
+ * Mainly use whenever they must be mixed all together in a generic type such as a {@link java.util.Set} or {@link java.util.Map} because
+ * they can be handled by {@link ValueAccessPointSet} and {@link ValueAccessPointMap} thanks to {@link ValueAccessPointComparator}.
  *
  * @author Guillaume Mary
  * @see ValueAccessPointComparator
+ * @see ValueAccessPointSet
+ * @see ValueAccessPointMap
  */
 public interface ValueAccessPoint {
 	
